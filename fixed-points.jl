68b504a825e3af2fb7e71db086d5190c9d2ae80f
swishtail <92322890+swishtail@users.noreply.github.com>
2025-05-03 23:55:44 +0200

    Update fixed-points.jl

--- fixed-points.jl
+++ fixed-points.jl
@@ -23,8 +23,9 @@ function logistic_fixed_points(r_lower, r_upper, r_step, iterations, tail, toler
 end
 
 # For rendering with CairoMakie, best to set r-step and quantisation to twice desired resolution
-# e.g. r-step    = (3.88 - 3.8)/8000 = 0.00001
-#      tolerance = (0.6 - 0.4)/6000 ~= 0.00003
+# e.g. for plot dimensions of 4000 x 3000 and axis limits of 3.8 < r < 3.88 and 0.4 < x < 0.6:
+# r-step    = (3.88 - 3.8)/8000 = 0.00001
+# tolerance = (0.6 - 0.4)/6000 ~= 0.00003
 the_points = logistic_fixed_points(3.8, 3.88, 0.00001, 10000, 4000, 0.00003)
 r_vals = first.(the_points)
 x_vals = last.(the_points)
@@ -32,6 +33,9 @@ x_vals = last.(the_points)
 fig = Figure(backgroundcolor = :black)
 ax = Axis(fig[1, 1], limits=(3.8, 3.88, 0.4, 0.6), width=4000, height=3000, backgroundcolor = :black)
 
+scatter!(ax, r_vals, x_vals; color = :white, markersize = 1)
+resize_to_layout!(fig)
+# save("logistic_fixed_points.png", fig; px_per_unit = 1)
 scatter!(ax, r_vals, x_vals; color = :white, markersize = 1)
 resize_to_layout!(fig)
 # save("logistic_fixed_points.png", fig; px_per_unit = 1)
